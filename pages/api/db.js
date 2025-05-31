1fa45dfbf0a422597a06a8cc1e9148b87aab0b70
Rene <rene.glavanovits@clouddna.at>
2025-05-31 09:58:49 +0200

    db user changed

--- pages/api/db.js
+++ pages/api/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 // Datenbankverbindung konfigurieren
 const pool = mysql.createPool({
   host: '10.0.1.217',     // MySQL Host
-  user: 'root',  // MySQL Benutzer
+  user: 'appuser',  // MySQL Benutzer
   password: 'root', // Passwort
   database: 'todo_db',   // Datenbankname
   waitForConnections: true,
